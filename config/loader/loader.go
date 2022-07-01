0f8de8c7229b456ef8d9716b90f225d798b4ea46
Brandon Kurtz <kurtz.brandon@gmail.com>
2022-07-01 17:13:25 -0400

    s3 store implementation

--- config/loader/loader.go
+++ config/loader/loader.go
@@ -2,7 +2,6 @@ package loader
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,17 +10,25 @@ import (
 
 	"github.com/discentem/pantri_but_go/stores"
 	localstore "github.com/discentem/pantri_but_go/stores/local"
+	"github.com/discentem/pantri_but_go/stores/s3"
 	"github.com/mitchellh/go-homedir"
 	"github.com/mitchellh/mapstructure"
 )
 
-func Initalize(sourceRepo, backend, address string, opts stores.Options) error {
-	if backend != "local" {
-		return fmt.Errorf("%s is not supported. only %q is currently supported", backend, "local")
-	}
-	_, err := localstore.New(sourceRepo, address, opts)
-	if err != nil {
-		return err
+func Initialize(sourceRepo, backend, address string, opts stores.Options) error {
+	switch b := (backend); b {
+	case "local":
+		_, err := localstore.New(sourceRepo, address, opts)
+		if err != nil {
+			return err
+		}
+	case "s3":
+		_, err := s3.New(sourceRepo, address, opts)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("%s is not a supported store type", b)
 	}
 	return nil
 
@@ -30,6 +37,15 @@ func Initalize(sourceRepo, backend, address string, opts stores.Options) error {
 func LoadLocalStore(m map[string]interface{}) (stores.Store, error) {
 	log.Printf("type %q detected in pantri %q", m["type"], m["pantri_address"])
 	var s *localstore.Store
+	if err := mapstructure.Decode(m, &s); err != nil {
+		return nil, err
+	}
+	return stores.Store(s), nil
+}
+
+func LoadS3Store(m map[string]interface{}) (stores.Store, error) {
+	log.Printf("type %q detected in pantri %q", m["type"], m["pantri_address"])
+	var s *s3.Store
 	if err := mapstructure.Decode(m, &s); err != nil {
 		return nil, err
 	}
@@ -65,8 +81,12 @@ func Load(sourceRepo string) (stores.Store, error) {
 	if err := json.Unmarshal(b, &m); err != nil {
 		return nil, err
 	}
-	if m["type"] == "local" {
+	switch t := (m["type"]); t {
+	case "local":
 		return LoadLocalStore(m)
+	case "s3":
+		return LoadS3Store(m)
+	default:
+		return nil, fmt.Errorf("%s is not a support store type", t)
 	}
-	return nil, errors.New("only local stores are supported for now")
 }
--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,6 +1,14 @@
 package metadata
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -8,4 +16,56 @@ type ObjectMetaData struct {
 	Name         string    `json:"name"`
 	Checksum     string    `json:"checksum"`
 	DateModified time.Time `json:"date_modified"`
+}
+
+func SHA256FromBytes(b []byte) (string, error) {
+	hash := sha256.New()
+	if _, err := io.Copy(hash, bytes.NewReader(b)); err != nil {
+		return "", err
+	}
+	h := hex.EncodeToString(hash.Sum(nil))
+	return h, nil
+}
+
+func GenerateFromReader(name string, modTime time.Time, r io.Reader) (*ObjectMetaData, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	if err != nil {
+		return nil, err
+	}
+	hash, err := SHA256FromBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return &ObjectMetaData{
+		Name:         name,
+		Checksum:     hash,
+		DateModified: modTime,
+	}, nil
+}
+
+func GenerateFromFile(f os.File) (*ObjectMetaData, error) {
+	fstat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	return GenerateFromReader(fstat.Name(), fstat.ModTime(), &f)
+}
+
+func ParsePfile(obj string) (*ObjectMetaData, error) {
+	pfile, err := os.Open(fmt.Sprintf("%s.pfile", obj))
+	if err != nil {
+		return nil, err
+	}
+	b, err := ioutil.ReadAll(pfile)
+	if err != nil {
+		return nil, err
+	}
+	var metadata *ObjectMetaData
+	if err := json.Unmarshal(b, &metadata); err != nil {
+		return nil, err
+	}
+	return metadata, nil
 }

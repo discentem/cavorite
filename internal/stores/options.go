9d5768898330b9fcfbb994390f777a81cdd28022
Ryan Diers <ryan.diers@airbnb.com>
2023-04-30 22:43:52 -0700

    refactor store to use shared interface, custom S3Store structs/opts, and wire together...
    rework internal folder to prevent import cycle and wire up main.go

---
+++ internal/stores/options.go
@@ -0,0 +1,6 @@
+package stores
+
+type Options struct {
+	PantriAddress         string `json:"pantri_address" mapstructure:"pantri_address"`
+	MetaDataFileExtension string `json:"metadata_file_extension" mapstructure:"metadata_file_extension"`
+}
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -10,9 +12,8 @@ import (
 	"github.com/google/logger"
 	"github.com/spf13/afero"
 
-	pantri "github.com/discentem/pantri_but_go/pantri/loader"
-
-	"github.com/discentem/pantri_but_go/stores"
+	"github.com/discentem/pantri_but_go/internal/config"
+	"github.com/discentem/pantri_but_go/internal/stores"
 
 	"github.com/urfave/cli/v2"
 )
@@ -50,84 +51,126 @@ func main() {
 			Value:    false,
 			Usage:    "displays logger.V(2).* messages",
 		},
+		&cli.StringFlag{
+			Name:     "pantri_address",
+			Aliases:  []string{"p"},
+			Required: true,
+			Usage:    "path to pantri storage",
+		},
+		&cli.StringFlag{
+			Name:     "metadata_file_extension",
+			Required: false,
+			Aliases:  []string{"ext"},
+			Usage:    "extension for object metadata files",
+			Value:    "pfile",
+		},
+		&cli.StringFlag{
+			Name:     "aws_region",
+			Required: false,
+			Aliases:  []string{"region"},
+			Usage:    "optional: AWS region: defaults to us-east-1",
+			Value:    "us-east-1",
+		},
 	}
 	app := &cli.App{
 		Flags: flags,
 		Usage: `pantri: but in go!`,
 		Commands: []*cli.Command{
 			{
-				Name:    "init",
-				Aliases: []string{},
-				Usage:   "Initalize pantri.",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:     "pantri_address",
-						Aliases:  []string{"p", "pa"},
-						Required: true,
-						Usage:    "path to pantri storage",
-					},
-					&cli.StringFlag{
-						Name:     "metadata_file_extension",
-						Required: false,
-						Aliases:  []string{"ext"},
-						Usage:    "extension for object metadata files",
-						Value:    "pfile",
-					},
-				},
+				Name:  "init",
+				Usage: "Initialize pantri.",
 				Action: func(c *cli.Context) error {
 					setLoggerOpts(c)
 
-					remove := c.Bool("remove")
 					var backend string
 					if c.NArg() == 0 {
-						log.Printf("defaulting to %s for pantri storage", defaultStore)
-						backend = defaultStore
+						log.Printf("defaulting to %s for pantri storage", stores.StoreTypeS3.String())
+						backend = stores.StoreTypeS3.String()
 					} else if c.NArg() == 1 {
 						backend = c.Args().First()
 					} else {
 						return errors.New("specifying multiple backends not allowed, try again")
 					}
+
 					fileExt := c.String("metadata_file_extension")
-					opts := stores.Options{
-						RemoveFromSourceRepo:  &remove,
-						MetaDataFileExtension: fileExt,
-					}
 					sourceRepo := c.String("source_repo")
 					pantriAddress := c.String("pantri_address")
-					// store agnostic initialization, specific initialization determined by backend
-					err := pantri.Initialize(context.Background(), afero.NewOsFs(), sourceRepo, backend, pantriAddress, opts)
-					if err != nil {
-						return err
+					awsRegion := c.String("aws_region")
+
+					opts := stores.Options{
+						PantriAddress:         pantriAddress,
+						MetaDataFileExtension: fileExt,
 					}
-					return nil
+
+					ctx := context.Background()
+					fsys := afero.NewOsFs()
+
+					// store agnostic initialization, specific initialization determined by backend
+					var storeType stores.StoreType
+					var cfg config.Config
+					switch storeType.FromString(backend) {
+					case stores.StoreTypeS3:
+						cfg = config.InitializeStoreTypeS3Config(
+							ctx,
+							fsys,
+							sourceRepo,
+							pantriAddress,
+							awsRegion,
+							opts,
+						)
+					default:
+						return config.ErrUnsupportedStore
+					}
+					return cfg.Write(fsys, sourceRepo)
 				},
 			},
 			{
 				Name:    "upload",
 				Aliases: []string{"u"},
 				Usage:   "Upload the specified file",
-				Flags: []cli.Flag{
-					&cli.BoolFlag{
-						Name:  "remove",
-						Value: false,
-						Usage: "Remove the file from local sourceRepo if present",
-					},
-				},
 				Action: func(c *cli.Context) error {
 					setLoggerOpts(c)
+					var store stores.Store
+					var cfg config.Config
 
 					if c.NArg() == 0 {
 						return errors.New("you must pass the path of an object to upload")
 					}
+					// Retrieve required and optional flag values
+					awsRegion := c.String("region")
 					sourceRepo := c.String("source_repo")
+
+					ctx := context.Background()
 					fsys := afero.NewOsFs()
-					s, err := pantri.Load(fsys, sourceRepo)
+					if sourceRepo == "" {
+						return errors.New("sourceRepo must be specified")
+					}
+
+					b, err := config.ReadConfig(fsys, sourceRepo)
+					if err != nil {
+						return errors.New("")
+					}
+					err = json.Unmarshal(b, &cfg)
 					if err != nil {
 						return err
 					}
-					// TODO(discentem) improve log message to include pantriAddress
-					logger.Infof("Uploading %s", c.Args().Slice())
-					if err := s.Upload(context.Background(), fsys, sourceRepo, c.Args().Slice()...); err != nil {
+
+					opts := cfg.Options
+
+					switch cfg.StoreType {
+					case stores.StoreTypeS3:
+						s3, err := stores.NewS3StoreClient(ctx, fsys, awsRegion, sourceRepo, opts)
+						if err != nil {
+							return fmt.Errorf("improper stores.S3Client init: %v", err)
+						}
+						store = stores.Store(s3)
+					default:
+						return fmt.Errorf("type %s is not supported", cfg.StoreType.String())
+					}
+
+					logger.Infof("Uploading to: %s", store.GetOptions().PantriAddress)
+					logger.Infof("Uploading file: %s", c.Args().Slice())
+					if err := store.Upload(ctx, sourceRepo, c.Args().Slice()...); err != nil {
 						return err
 					}
 					return nil
@@ -139,19 +182,47 @@ func main() {
 				Usage:   "Retrieve the specified file",
 				Action: func(c *cli.Context) error {
 					setLoggerOpts(c)
+					var store stores.Store
+					var cfg config.Config
 
 					if c.NArg() == 0 {
 						return errors.New("you must pass the path of an object to retrieve")
 					}
+					// Retrieve required and optional flag values
+					awsRegion := c.String("region")
 					sourceRepo := c.String("source_repo")
+
+					ctx := context.Background()
 					fsys := afero.NewOsFs()
-					s, err := pantri.Load(fsys, sourceRepo)
+					if sourceRepo == "" {
+						return errors.New("sourceRepo must be specified")
+					}
+
+					b, err := config.ReadConfig(fsys, sourceRepo)
+					if err != nil {
+						return errors.New("")
+					}
+					err = json.Unmarshal(b, &cfg)
 					if err != nil {
 						return err
 					}
-					// TODO(discentem) improve log message to include pantriAddress
-					log.Printf("Retrieving %s", c.Args().Slice())
-					if err := s.Retrieve(context.Background(), fsys, sourceRepo, c.Args().Slice()...); err != nil {
+
+					opts := cfg.Options
+
+					switch cfg.StoreType {
+					case stores.StoreTypeS3:
+						s3, err := stores.NewS3StoreClient(ctx, fsys, awsRegion, sourceRepo, opts)
+						if err != nil {
+							return fmt.Errorf("improper stores.S3Client init: %v", err)
+						}
+						store = stores.Store(s3)
+					default:
+						return fmt.Errorf("type %s is not supported", cfg.StoreType.String())
+					}
+
+					logger.Infof("Downloading files from: %s", store.GetOptions().PantriAddress)
+					logger.Infof("Downloading file: %s", c.Args().Slice())
+					if err := store.Retrieve(ctx, sourceRepo, c.Args().Slice()...); err != nil {
 						return err
 					}
 					return nil
@@ -167,7 +238,6 @@ func main() {
 			},
 		},
 	}
-
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)

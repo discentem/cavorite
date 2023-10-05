3013105660a8fca765724893068c37be7e46d372
BK <brandonk@uber.com>
2023-10-05 15:19:59 -0700

    make sure RealExecutor meets interface

--- exec/exec.go
+++ exec/exec.go
@@ -7,15 +7,24 @@ import (
 )
 
 type Executor interface {
-	Command() *exec.Cmd
+	Command(string, ...string)
 	Start() error
 	Wait() error
 	StdoutPipe() (io.ReadCloser, error)
 	StderrPipe() (io.ReadCloser, error)
 }
 
+var (
+	_ = Executor(&RealExecutor{})
+)
+
 type RealExecutor struct {
 	*exec.Cmd
+}
+
+func (e *RealExecutor) Command(bin string, args ...string) {
+	e.Cmd.Path = bin
+	e.Cmd.Args = args
 }
 
 func WriteOutput(in io.ReadCloser, post io.WriteCloser) error {

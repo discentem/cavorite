9cbf7a74ece3531f47f3423ca7d3a1d74bae6034
BK <brandonk@uber.com>
2023-10-05 16:29:43 -0700

    test for exec.Stream()

--- exec/exec.go
+++ exec/exec.go
@@ -27,6 +27,33 @@ func (e *RealExecutor) Command(bin string, args ...string) {
 	e.Cmd.Args = args
 }
 
+func (e *RealExecutor) Stream(posters ...io.WriteCloser) error {
+	stdout, err := e.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := e.StderrPipe()
+	if err != nil {
+		return err
+	}
+	inputPipes := []io.ReadCloser{stdout, stderr}
+
+	if err := e.Start(); err != nil {
+		return err
+	}
+	for _, pipe := range inputPipes {
+		for _, post := range posters {
+			//nolint:errcheck
+			go WriteOutput(pipe, post)
+		}
+	}
+
+	if err := e.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func WriteOutput(in io.ReadCloser, post io.WriteCloser) error {
 	r := bufio.NewScanner(in)
 	for r.Scan() {
---
+++ exec/test/artifacts/long_running.sh
@@ -0,0 +1,5 @@
+for i in {1..3};
+do
+    echo "$i "
+    sleep 1
+done

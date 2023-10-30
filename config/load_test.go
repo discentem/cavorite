980d432d3435c61d752c7b2151d53fb48cfa2340
BK <brandonk@uber.com>
2023-10-30 15:41:16 -0700

    add more tests for config/load.go

--- config/load_test.go
+++ config/load_test.go
@@ -14,34 +14,128 @@ import (
 // to be read and parsed by viper
 // Test inspired from https://github.com/spf13/viper/blob/master/viper_test.go
 func TestLoadConfig(t *testing.T) {
-	fs := afero.NewMemMapFs()
+	tests := []struct {
+		name              string
+		parseInto         *Config
+		fsys              afero.Fs
+		expected          *Config
+		expectedLoadError func(e error) bool
+	}{
+		{
+			name:      "valid config is parsed correctly",
+			parseInto: &Config{},
+			// creating an fsys with a valid cavorite config
+			fsys: func(t *testing.T) afero.Fs {
+				fs := afero.NewMemMapFs()
 
-	err := fs.Mkdir(".cavorite", 0o777)
-	require.NoError(t, err)
+				err := fs.Mkdir(".cavorite", 0o777)
+				require.NoError(t, err)
 
-	file, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
-	require.NoError(t, err)
+				f, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
+				require.NoError(t, err)
 
-	cfg := Config{}
+				_, err = f.Write([]byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+					}
+				   }`),
+				)
+				require.NoError(t, err)
+				f.Close()
+				return fs
+			}(t),
+			expected: &Config{
+				StoreType: stores.StoreType("s3"),
+				Options: stores.Options{
+					BackendAddress:        "s3://blahaddress/bucket",
+					MetadataFileExtension: "",
+					Region:                "us-east-9876",
+				},
+			},
+			expectedLoadError: func(e error) bool {
+				return e == nil
+			},
+		},
+		{
+			name:      "invalid config parsing returns error",
+			parseInto: nil,
+			fsys: func(t *testing.T) afero.Fs {
+				fs := afero.NewMemMapFs()
 
-	_, err = file.Write([]byte(`{
-		"store_type": "s3",
-		"options": {
-		 "backend_address": "s3://blahaddress/bucket",
-		 "metadata_file_extension": "",
-		 "region": "us-east-9876"
-		}
-	   }`),
-	)
-	require.NoError(t, err)
-	file.Close()
+				err := fs.Mkdir(".cavorite", 0o777)
+				require.NoError(t, err)
 
-	err = Load(fs, &cfg)
-	assert.NoError(t, err)
+				f, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
+				require.NoError(t, err)
 
-	assert.Equal(t, stores.StoreType("s3"), cfg.StoreType)
-	assert.Equal(t, "s3://blahaddress/bucket", cfg.Options.BackendAddress)
-	assert.Equal(t, "", cfg.Options.MetadataFileExtension)
-	assert.Equal(t, "us-east-9876", cfg.Options.Region)
+				// "options" is missing the closing curly bracket below
+				// so this is expected to cause ErrViperReadConfig
+				_, err = f.Write([]byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+				   }`),
+				)
+				require.NoError(t, err)
+				f.Close()
+				return fs
+			}(t),
+			expectedLoadError: func(e error) bool {
+				return assert.ErrorIs(t, e, ErrViperReadConfig)
+			},
+		},
+		{
+			name: "no cfgDest passed, parse into global cfg variable",
+			parseInto: func() *Config {
+				return &Cfg
+			}(),
+			fsys: func(t *testing.T) afero.Fs {
+				fs := afero.NewMemMapFs()
 
+				err := fs.Mkdir(".cavorite", 0o777)
+				require.NoError(t, err)
+
+				f, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
+				require.NoError(t, err)
+				_, err = f.Write([]byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+					}
+				   }`),
+				)
+				require.NoError(t, err)
+				f.Close()
+				return fs
+			}(t),
+			expectedLoadError: func(e error) bool {
+				return e == nil
+			},
+			expected: &Config{
+				StoreType: stores.StoreType("s3"),
+				Options: stores.Options{
+					BackendAddress:        "s3://blahaddress/bucket",
+					MetadataFileExtension: "",
+					Region:                "us-east-9876",
+				},
+			},
+		},
+	}
+
+	t.Parallel()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := Load(test.fsys, test.parseInto)
+			assert.Equal(t, test.expectedLoadError(err), true)
+			assert.Equal(t, test.expected, test.parseInto)
+		})
+
+	}
 }

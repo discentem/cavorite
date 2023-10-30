b12f1907e1c924b4e1bf7749a9a65cc7fd7fdc56
BK <brandonk@uber.com>
2023-10-30 16:25:07 -0700

    add more tests for config/load.go

--- config/load_test.go
+++ config/load_test.go
@@ -10,38 +10,105 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func FsysWithJsonCavoriteConfig(t *testing.T, b []byte) afero.Fs {
+	t.Helper()
+	fs := afero.NewMemMapFs()
+	err := fs.Mkdir(".cavorite", 0o777)
+	require.NoError(t, err)
+
+	f, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
+	require.NoError(t, err)
+	_, err = f.Write(b)
+	require.NoError(t, err)
+	return fs
+}
+
 // TestLoadConfig creates a pantri config file in memory
 // to be read and parsed by viper
 // Test inspired from https://github.com/spf13/viper/blob/master/viper_test.go
 func TestLoadConfig(t *testing.T) {
-	fs := afero.NewMemMapFs()
+	tests := []struct {
+		name              string
+		parseInto         *Config
+		fsys              afero.Fs
+		expected          *Config
+		expectedLoadError func(e error) bool
+	}{
+		{
+			name:      "valid config is parsed correctly",
+			parseInto: &Config{},
+			// creating an fsys with a valid cavorite config
+			fsys: FsysWithJsonCavoriteConfig(t, []byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+					}
+				   }`)),
+			expected: &Config{
+				StoreType: stores.StoreType("s3"),
+				Options: stores.Options{
+					BackendAddress:        "s3://blahaddress/bucket",
+					MetadataFileExtension: "",
+					Region:                "us-east-9876",
+				},
+			},
+			expectedLoadError: func(e error) bool {
+				return e == nil
+			},
+		},
+		{
+			name:      "invalid config parsing returns error",
+			parseInto: nil,
+			// "options" is missing the closing curly bracket below
+			// so this is expected to cause ErrViperReadConfig
+			fsys: FsysWithJsonCavoriteConfig(t, []byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+				   }`)),
+			expectedLoadError: func(e error) bool {
+				return assert.ErrorIs(t, e, ErrViperReadConfig)
+			},
+		},
+		{
+			name: "no cfgDest passed, parse into global cfg variable",
+			parseInto: func() *Config {
+				return &Cfg
+			}(),
+			fsys: FsysWithJsonCavoriteConfig(t, []byte(`{
+					"store_type": "s3",
+					"options": {
+					 "backend_address": "s3://blahaddress/bucket",
+					 "metadata_file_extension": "",
+					 "region": "us-east-9876"
+					}
+				   }`),
+			),
+			expectedLoadError: func(e error) bool {
+				return e == nil
+			},
+			expected: &Config{
+				StoreType: stores.StoreType("s3"),
+				Options: stores.Options{
+					BackendAddress:        "s3://blahaddress/bucket",
+					MetadataFileExtension: "",
+					Region:                "us-east-9876",
+				},
+			},
+		},
+	}
 
-	err := fs.Mkdir(".cavorite", 0o777)
-	require.NoError(t, err)
+	t.Parallel()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := Load(test.fsys, test.parseInto)
+			assert.Equal(t, test.expectedLoadError(err), true)
+			assert.Equal(t, test.expected, test.parseInto)
+		})
 
-	file, err := fs.Create(testutils.AbsFilePath(t, ".cavorite/config"))
-	require.NoError(t, err)
-
-	cfg := Config{}
-
-	_, err = file.Write([]byte(`{
-		"store_type": "s3",
-		"options": {
-		 "backend_address": "s3://blahaddress/bucket",
-		 "metadata_file_extension": "",
-		 "region": "us-east-9876"
-		}
-	   }`),
-	)
-	require.NoError(t, err)
-	file.Close()
-
-	err = Load(fs, &cfg)
-	assert.NoError(t, err)
-
-	assert.Equal(t, stores.StoreType("s3"), cfg.StoreType)
-	assert.Equal(t, "s3://blahaddress/bucket", cfg.Options.BackendAddress)
-	assert.Equal(t, "", cfg.Options.MetadataFileExtension)
-	assert.Equal(t, "us-east-9876", cfg.Options.Region)
-
+	}
 }
